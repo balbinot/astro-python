a3a56e4f1a921a147d66c5b3c4900d71453cbe5d
Michael Gordon <gordon@astro.umn.edu>
2014-09-15 10:42:56 -0500

    point

---
+++ point_align.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+import pyfits
+from scipy.ndimage.interpolation import shift
+from centroid import centroid
+import argparse
+
+def align(filename,coords,rad):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Align input images given an initial guess.')
+    
+    parser.add_argument('filelist',nargs='+',help='List of filenames for alignment.')
+    parser.add_argument('--coords',nargs=2,type=list,help='Initial guess of coordinates: x y')
+    parser.add_argument('-r',type=int,default=40,help='Search radius (default is 40 pixels)')
+    
+    args = parser.parse_args()
+
+    for filename in args.filelist:
+        align(filename,args.coords,args.r)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
